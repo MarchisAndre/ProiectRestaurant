a065f6d74518c29f23203a6a77d7ba4198ca6041
MarchisAndre <amarchis03@gmail.com>
2024-04-04 20:29:53 +0300

    initial commit

---
+++ paginaweb/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HomePage from './core/components/HomePage'
+import styles from './App.module.scss'
+function App() {
+
+  return (
+    <>
+      <HomePage/>
+    </>
+  )
+}
+
+export default App
---
+++ paginaweb/src/core/components/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from './HomePage.module.scss'
+
+const HomePage=()=>{
+    return <div className={styles.mainContainer}>
+        <div className={styles.menu}>
+        logo
+        </div>
+        <div className={styles.landing}>
+        poze
+        </div>
+    </div>
+}
+
+export default HomePage
